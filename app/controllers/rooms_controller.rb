48a9f158657c72b4698c5f21d943c615d1b90eb2
Adan Nega <shiryu200199@hotmail.com>
2023-01-06 05:52:39 -0600

    Add : sending rt msgs & styling rt chatrooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,7 +3,23 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
+
     @users = User.all_except(current_user)
+    render 'index'
+  end
+
+  def show
+
+    @single_room = Room.find(params[:id])
+    @room = Room.new
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+
+    @message = Message.new
+    @messages = @single_room.messages.order(create_at: :asc)
+
+    render 'index'
+
   end
 
   def create
